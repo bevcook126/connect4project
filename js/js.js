333fbe2a7dd99642b4f928af4342673eeee9b4a7
username <bevcook126@gmail.com>
2022-07-01 13:32:06 -0700

    changing file

---
+++ js/js.js
@@ -0,0 +1,25 @@
+/*----- constants -----*/
+
+// board
+// columns
+
+/*----- app's state (variables) -----*/
+
+
+
+/*----- cached element references -----*/
+
+
+
+/*----- event listeners -----*/
+
+// hover -> turn semi-transparent
+// click
+// loop
+
+/*----- functions -----*/
+
+// render
+
+
+
